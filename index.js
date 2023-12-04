1cd33e90af626d1a95675d00fec8d76c0fd516a2
mezgo <mezgoes3567@hotmail.com>
2023-12-04 18:46:08 -0500

    Project completed
    Game loop has been implemented.
    score for player & computer added.

--- index.js
+++ index.js
@@ -39,21 +39,61 @@ function playRound(playerSelection, computerSelection) {
     console.log(results.get("computer"))
 
     if (results.get("player") == results.get("computer")) {
-        alert("Wow, it's even this time!!")
+        alert("Wow, it's a tie this time!!")
+        return "tie"
     }
     else if (Array.from(results.values()).includes("rock") && Array.from(results.values()).includes("scissors")) {
         let winner = getWinner(results, "rock")
         alert(winner + " wins")
+        return winner
     }
     else if (Array.from(results.values()).includes("rock") && Array.from(results.values()).includes("paper")) {
         let winner = getWinner(results, "paper")
         alert(winner + " wins")
+        return winner
     }
     else if (Array.from(results.values()).includes("paper") && Array.from(results.values()).includes("scissors")) {
         let winner = getWinner(results, "scissors")
         alert(winner + " wins")
+        return winner
     }
 }
 
-playRound(getPlayerChoice(), getComputerChoice())
+function game(){
+    let counter = 1,
+        player_score = 0,
+        computer_score = 0
+
+    while (counter <= 5){
+        winner = playRound(getPlayerChoice(), getComputerChoice())
+
+        if (winner == "player"){
+            player_score++
+        }
+        else if (winner == "computer"){
+            computer_score++
+        }
+        else{
+            counter--
+        }
+
+        console.log("player: " + player_score)
+        console.log("computer: " + computer_score)
+        console.log("counter: " + counter)
+
+        if (player_score == 3){
+            alert("player wins with a score of: " + player_score + " points." + " computer loses with a score of: " + computer_score + " points")
+            break
+        }
+        else if(computer_score == 3){
+            alert("computer wins with a score of: " + computer_score + " points." + " player loses with a score of: " + player_score + " points")
+            break
+        }
+        else{
+            counter++
+        }
+    }
+}
+
+game()
 
